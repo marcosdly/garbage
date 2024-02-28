587115db7fc5b6e4b3c3056b1c42829b5edb1696
Marcos Delay <dev.marcosdly@gmail.com>
2024-02-28 04:52:08 -0300

    chore: dimensions and position (master view)

--- src/gui/window.py
+++ src/gui/window.py
@@ -2,12 +2,19 @@ import tkinter as t
 
 
 class Window(t.Frame):
-    def __init__(self, master: t.Tk) -> None:
+    def __init__(
+        self,
+        master: t.Tk,
+    ) -> None:
         super().__init__(master)
         self.master = master
         self.root = master
 
         self.root.wm_title("TKinter Test - My Program")
+        self.root.geometry("1280x768")
+        self.root.resizable(width=False, height=False)
+        self.root.grid()
+        self.grid(column=0, row=0, sticky=t.N + t.S + t.W + t.E)
 
 
 root = t.Tk()
