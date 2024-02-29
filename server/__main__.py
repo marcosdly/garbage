72d6fd0055e1636036be2aedbe77fcc8501a9730
Marcos Delay <dev.marcosdly@gmail.com>
2024-02-29 12:59:24 -0300

    chore: basic server

---
+++ server/__main__.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    from server.app import app
+    import server.common_routes
+
+    print("running")
+    app.run()
---
+++ server/common_routes.py
@@ -0,0 +1,20 @@
+from server.app import app
+from quart import Response
+
+
+@app.route("/")
+async def index():
+    with open("client/index.html", "rt") as html:
+        return html.read()
+
+
+@app.route("/index.js")
+async def js():
+    with open("client/index.js", "rt") as js:
+        return Response(response=js.read(), content_type="text/javascript")
+
+
+@app.route("/style.css")
+async def css():
+    with open("client/style.css", "rt") as css:
+        return Response(response=css.read(), content_type="text/css")
