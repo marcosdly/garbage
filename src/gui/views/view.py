9c48941e941fa332daa9445621a49ad658602f22
Marcos Delay <dev.marcosdly@gmail.com>
2024-03-03 18:13:04 -0300

    chore: remove old code structure

--- src/gui/views/view.py
+++
@@ -1,8 +0,0 @@
-import tkinter as t
-
-
-class View(t.Frame):
-    def __init__(self, parent, **kw) -> None:
-        super().__init__(parent, **kw)
-        self.parent = parent
-        self.grid_configure(column=0, row=1, sticky=t.N + t.E + t.S + t.W)
--- src/video/main.py
+++
@@ -1,7 +0,0 @@
-def main(frame_buffer) -> None:
-    from multiprocessing import SimpleQueue
-    from typing import cast
-    from src.video.transmit import transmit_video
-
-    frame_buffer = cast(SimpleQueue, frame_buffer)
-    transmit_video(frame_buffer)
